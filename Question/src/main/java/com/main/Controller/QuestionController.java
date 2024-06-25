b98129706b10c8a8ffda63dd91b2bcf03461eeca
arjundhatbale <arjundhatbale321@gmail.com>
2024-06-25 23:49:16 +0530

    Update Question Controller with Response Entity

--- Question/src/main/java/com/main/Controller/QuestionController.java
+++ Question/src/main/java/com/main/Controller/QuestionController.java
@@ -2,7 +2,10 @@ package com.main.Controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +20,8 @@ import com.main.Service.QuestoinService;
 @RequestMapping("/question")
 public class QuestionController {
 
+	@Autowired
+	private Logger logger;
 	
 	@Autowired
 	private QuestoinService questoinService; 
@@ -27,26 +32,35 @@ public class QuestionController {
 	
 	
 	@PostMapping("/")
-	public Question save(@RequestBody Question question) {
-		return this.questoinService.add(question);
+	public ResponseEntity<Question> save(@RequestBody Question question) {
+		
+		logger.info("Request receive to Save Question...");
+		
+		return new ResponseEntity<Question>(this.questoinService.add(question),HttpStatus.CREATED);
+
 	}
 	
 	@GetMapping("/{questionId}")
-	public Question getSingleQuestion(@PathVariable Long questionId) {
+	public ResponseEntity<Question> getSingleQuestion(@PathVariable Long questionId) {
+		logger.info("Request Receive to Get Question By Question Id...");
 		Question question = this.questoinService.get(questionId);
-		return question;
+		return new ResponseEntity<>(question,HttpStatus.ACCEPTED);
 	}
 	
 	@GetMapping("/")
-	public List<Question> getAll(){
-		return this.questoinService.get();
+	public  ResponseEntity<List<Question>> getAll(){
+		logger.info("Request Receive to Get All Questions...");
+		 
+		return new ResponseEntity<>(this.questoinService.get(),HttpStatus.ACCEPTED);
 	}
 
 //  get all question of specific quiz
 	
 	@GetMapping("/quiz/{quizId}")
-	public List<Question> getQuestionsOfQuiz(@PathVariable Long quizId){
-		return this.questoinService.getQuestionsOfQuiz(quizId);
+	public ResponseEntity<List<Question>> getQuestionsOfQuiz(@PathVariable Long quizId){
+		logger.info("Request Receive to get Question of Quize by quize Id ...");
+//		return this.questoinService.getQuestionsOfQuiz(quizId);
+		return new ResponseEntity<>(this.questoinService.getQuestionsOfQuiz(quizId),HttpStatus.ACCEPTED);
 	}
 	
 }
